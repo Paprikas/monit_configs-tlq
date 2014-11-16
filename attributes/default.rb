beca4f150c8ef4ec043cdd3492a6971c67899bbb
Max Paprikas <kombise@gmail.com>
2014-11-16 16:42:27 +0500

    Move system settings to attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,8 @@ default['monit_configs-tlq']['postgres']['pidfile'] = '/var/run/postgresql/9.3-m
 default['monit_configs-tlq']['mongo']['pidfile'] = '/var/run/mongo/mongo.pid'
 
 default['redisio']['servers'] = [{'port' => '6379'}]
+
+default['monit_configs-tlq']['system']['memory_usage'] = '75'
+default['monit_configs-tlq']['system']['cpu_usage_user'] = '70'
+default['monit_configs-tlq']['system']['cpu_usage_system'] = '30'
+default['monit_configs-tlq']['system']['space_usage'] = '50'
